a32459d8db21f9062ed4389b7cb50ff463b29a5c
cristhian <cristhian.ramos@vallegrande.edu.pe>
2018-11-15 12:04:02 -0500

    he agregado mas ejercicios en hola mundo

---
+++ Miweb-/lab01/JS/hola  mundo .js
@@ -0,0 +1,33 @@
+//mi primer mensaje por
+ console.log("hola terricola ... te escribo desde el mas alla ...")
+
+ //imprimir otro mensaje de prueba
+ console.log("este es otro mensaje ... no te olvides como hecerlo....")
+
+ //imprimir numeros
+ console.log("5.900")
+
+ //imprimir 5 nombre de destritos
+ console.log("imperial , san vicente , mala , lima , asia ")
+
+ //suma los numeros 100 , 5000
+ console.log("100 + 500")
+
+ //comparar los siguientes numeros 800 y 500
+ console.log("800 < 500")
+
+ //imprime 3  nombre de paise 
+ console.log("peru , brazil , canada ")
+
+ // imprimer mensaje de erro
+ console.error("este erro es de prueba .... no te asuste todo vas estar super bien ")
+
+ //imprimer mensaje de advertencias
+ console.warn("cuidado... te estoy vigilando ");
+ 
+ //imprimer mensaje de informacion
+ console.info("este es un aviso de javascript")
+
+ //multiplicar 3 numeros:10 x 25 x 105
+ console.log("10*15*105")
+ 
